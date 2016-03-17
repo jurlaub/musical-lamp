23ee063bdf7edcce6a559fde5534348bdbfa5a3f
jurlaub <j.urlaub@gmail.com>
2016-03-16 22:56:27 -0700

    working on Joke, SimpleJokeBook to provide placeholder jokes.

---
+++ jokelibrary/src/main/java/com/example/Joke.java
@@ -0,0 +1,47 @@
+package com.example;
+
+/**
+ * Created by dev on 3/16/16.
+ */
+public class Joke {
+
+    private String jokeID;
+    private String jokeQuestion;
+    private String jokeAnswer;
+
+    public Joke(String jokeID) {
+        this.jokeID = jokeID;
+    }
+
+    public Joke(String jokeID, String jokeQuestion, String jokeAnswer) {
+        this.jokeID = jokeID;
+        this.jokeQuestion = jokeQuestion;
+        this.jokeAnswer = jokeAnswer;
+    }
+
+
+
+    public String getJokeID() {
+        return jokeID;
+    }
+
+    public void setJokeID(String jokeID) {
+        this.jokeID = jokeID;
+    }
+
+    public String getJokeQuestion() {
+        return jokeQuestion;
+    }
+
+    public void setJokeQuestion(String jokeQuestion) {
+        this.jokeQuestion = jokeQuestion;
+    }
+
+    public String getJokeAnswer() {
+        return jokeAnswer;
+    }
+
+    public void setJokeAnswer(String jokeAnswer) {
+        this.jokeAnswer = jokeAnswer;
+    }
+}
---
+++ jokelibrary/src/main/java/com/example/SimpleJokeBook.java
@@ -0,0 +1,81 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Created by dev on 3/16/16.
+ */
+public class SimpleJokeBook {
+
+    private static SimpleJokeBook sSimpleJokeBook = null;
+    private ArrayList<Joke> mJokes;
+    private Random mRandom;
+
+    protected SimpleJokeBook(){
+        mJokes = new ArrayList<>();
+        mRandom = new Random();
+
+        generateStaticJokes();
+    }
+
+    public static SimpleJokeBook getInstance(){
+        if(sSimpleJokeBook == null) {
+            sSimpleJokeBook = new SimpleJokeBook();
+        }
+
+        return sSimpleJokeBook;
+    }
+
+
+    public ArrayList<Joke> getmJokes(){
+        return mJokes;
+    }
+
+
+    // get target joke
+    public Joke getJoke(int n){
+        return mJokes.get(n);
+    }
+
+    // get a random joke based on a pseudorandom number.
+    public Joke getRandomJoke(){
+        int targetJoke = mRandom.nextInt(mJokes.size());
+
+        return mJokes.get(targetJoke);
+    }
+
+
+
+    // A class to generate a static set of jokes.
+    // special thanks to jokes on http://www.rd.com/jokes/riddles/
+    private void generateStaticJokes(){
+
+        Joke j1 = new Joke("j001",
+                "What is Purple, Green, and Squigilly?",
+                "Sad, I was hoping you could tell me... ");
+
+        mJokes.add(j1);
+
+        Joke j2 = new Joke("j002",
+                "With pointed fangs I sit and wait; with piercing force I crunch out fate; grabbing victims, proclaiming might; physically joining with a single bite. What am I?",
+                "A stapler");
+        mJokes.add(j2);
+
+        Joke j3 = new Joke("j003",
+                "What is easy to get into but hard to get out of?",
+                "Trouble");
+        mJokes.add(j3);
+
+        Joke j4 = new Joke("j004",
+                "I am the beginning of hte end, and the end of time and Space. I am essential to " +
+                        "creation, and I surround every place. Who am I?",
+                "The letter E.");
+        mJokes.add(j4);
+
+
+    }
+
+
+
+}
