fc276bbf865b01a5702b36e58512e39ef76865bb
jurlaub <j.urlaub@gmail.com>
2016-03-23 02:34:32 -0700

    initial google cloud endpoint

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,15 +1,24 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Intent;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.util.Pair;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
-import com.example.Joke;
 import com.example.JokeLibrary;
-import com.example.android.jokedisplay.DisplayActivity;
+import com.example.dev.cloudii.backend.myApi.MyApi;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -21,6 +30,8 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mJokeLibrary = new JokeLibrary();
+
+
     }
 
 
@@ -48,19 +59,67 @@ public class MainActivity extends ActionBarActivity {
 
     public void tellJoke(View view){
 
-        Joke joke = mJokeLibrary.getJoke();
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
+        Log.v("mainActivity", "In the tellJoke after async task");
 
+//        Joke joke = mJokeLibrary.getJoke();
 //
-//        Toast.makeText(this, joke.getJokeQuestion(), Toast.LENGTH_LONG).show();
-        Intent intent = new Intent(this, DisplayActivity.class);
+////
+////        Toast.makeText(this, joke.getJokeQuestion(), Toast.LENGTH_LONG).show();
+//        Intent intent = new Intent(this, DisplayActivity.class);
+//
+//        intent.putExtra(DisplayActivity.QUESTION_TEXT, joke.getJokeQuestion());
+//        intent.putExtra(DisplayActivity.ANSWER_TEXT, joke.getJokeAnswer());
+//
+//
+//        startActivity(intent);
+//
 
-        intent.putExtra(DisplayActivity.QUESTION_TEXT, joke.getJokeQuestion());
-        intent.putExtra(DisplayActivity.ANSWER_TEXT, joke.getJokeAnswer());
+    }
 
 
-        startActivity(intent);
 
 
+
+    private class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+        private  MyApi myApiService = null;
+        private Context context;
+
+        @Override
+        protected String doInBackground(Pair<Context, String>... params) {
+            if(myApiService == null) {  // Only do this once
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        // options for running against local devappserver
+                        // - 10.0.2.2 is localhost's IP address in Android emulator
+                        // - turn off compression when running against local devappserver
+                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+//                        .setRootUrl("http://192.168.0.221:8080/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+                // end options for devappserver
+
+                myApiService = builder.build();
+            }
+
+            context = params[0].first;
+            String name = params[0].second;
+
+            try {
+                return myApiService.sayHi(name).execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+        }
     }
 
 
